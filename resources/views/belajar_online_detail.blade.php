0540330324f24cec460e754d90075617c1bcda0e
Bayu <bayurz1001@gmail.com>
2024-04-24 13:29:28 +0700

    up

--- resources/views/belajar_online_detail.blade.php
+++ resources/views/belajar_online_detail.blade.php
@@ -35,7 +35,7 @@
                             <div class="card-body">
                                 <div class="doctor-widget">
                                     <div class="video-container">
-                                        <iframe width="700" height="415"
+                                        <iframe width="690" height="415"
                                             src="https://www.youtube.com/embed/TTimxxM9RbE" frameborder="0" allowfullscreen
                                             style="border-radius: 15px;"></iframe>
                                     </div>
