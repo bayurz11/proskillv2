b9a9e5b4ea6b1a047be490ef8f405a98b4e4841a
Bayu <bayurz1001@gmail.com>
2024-04-24 21:24:48 +0700

    update

--- resources/views/belajar_online_detail.blade.php
+++ resources/views/belajar_online_detail.blade.php
@@ -48,7 +48,7 @@
                             <div class="card-body">
                                 <ul>
                                     <li><a href="">Cara Membuat Foto Produk Melalui Beberapa Tools Ai</a></li>
-                                    <li><a href="">Judul 2</a></li>
+                                    <li><a href="">Cara Membuat Logo Maskot dengan Ai</a></li>
                                     <li><a href="">Judul 3</a></li>
                                     <li><a href="">Judul 4</a></li>
                                     <li><a href="">Judul 5</a></li>
