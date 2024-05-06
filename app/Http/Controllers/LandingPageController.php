ce50fab53ef539ded3f16c8771e9ff44b19f9c4c
Bayu <bayurz1001@gmail.com>
2024-05-06 22:05:30 +0700

    up

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\UserRoles;
+use App\Models\UserProfile;
+use Illuminate\Support\Facades\Auth;
 
 class LandingpageController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return view('index');
+    }
 }
