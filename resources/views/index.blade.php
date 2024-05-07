c57050f879d18b11aa4ebdd2593a919955d48a0b
Bayu <bayurz1001@gmail.com>
2024-05-07 13:13:10 +0700

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1080,18 +1080,19 @@
     </section>
 
     <!-- // Popup whatsapp// -->
-
     <div class="popup" id="popup"
         style="position: fixed; bottom: 20px; right: 20px; background-color: #25D366; color: #fff; padding: 10px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); display: none; z-index: 9999;">
-        <div style="position: absolute; top: 5px; right: 5px; cursor: pointer;"
-            onclick="document.getElementById('popup').style.display = 'none';">×</div>
-        <p>Butuh bantuan? <a href="https://wa.me/+6281266187125" target="_blank">Hubungi kami via WhatsApp!</a></p>
+        <img src="public/assets/img/Whatsapp.png" alt="WhatsApp Logo"
+            style="position: absolute; top: 5px; right: 5px; width: 20px; height: 20px; cursor: pointer;"
+            onclick="document.getElementById('popup').style.display = 'none';">
+        <p>Butuh bantuan? <a href="https://wa.me/+6281266187125" target="_blank"></a></p>
     </div>
 
     <script>
         // Automatically show the popup after 5 seconds (adjust as needed)
         setTimeout(function() {
             document.getElementById("popup").style.display = "block";
-        }, 5000);
+        }, 500);
     </script>
+
 @endsection
