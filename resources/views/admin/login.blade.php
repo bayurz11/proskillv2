d15c9bf866753cb4a0dea44a605ba4c5c28ccb77
Bayu <bayurz1001@gmail.com>
2024-05-07 20:16:02 +0700

    up

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -91,7 +91,7 @@
                                         <div class="mb-3">
                                             <label for="email" class="form-label">Email address</label>
                                             <input type="email" class="form-control" id="email" name="email"
-                                                placeholder="Email">
+                                                placeholder="Email" autofocus>
                                         </div>
                                         <div class="mb-3">
                                             <label for="password" class="form-label">Password</label>
