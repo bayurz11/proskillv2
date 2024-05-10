2ad0da6bb7f5a099115b29a0376ff7d9b1df4717
Bayu <bayurz1001@gmail.com>
2024-05-10 10:12:12 +0700

    up

--- resources/views/admin/artikel/artikel.blade.php
+++ resources/views/admin/artikel/artikel.blade.php
@@ -12,7 +12,7 @@
             </ol>
         </nav>
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
