b320fa37a4364014605f80562d9517952e1a92bf
Bayu <bayurz1001@gmail.com>
2024-05-10 13:47:36 +0700

    up

--- resources/views/admin/artikel/artikel.blade.php
+++ resources/views/admin/artikel/artikel.blade.php
@@ -114,8 +114,8 @@
                     <div class="card-body">
                         <h6 class="card-title">Tabel Artikel</h6>
                         <button type="button" class="btn btn-primary position-absolute top-0 end-0 mt-3 me-3"
-                            data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="mdi mdi-star">Tambah Artikel</i>
-
+                            data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="mdi mdi-star"></i>
+                            Tambah Artikel
                         </button>
                         <p class="text-muted mb-3">Jumlah Artikel :</p>
                         <div class="table-responsive">
