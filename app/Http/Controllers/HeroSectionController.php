f7f5093aee1edc10fca163b1aa2b78065b803bf2
Bayu <bayurz1001@gmail.com>
2024-05-11 10:04:30 +0700

    up

--- app/Http/Controllers/HeroSectionController.php
+++ app/Http/Controllers/HeroSectionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\HeroSection;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreHeroSectionRequest;
 use App\Http\Requests\UpdateHeroSectionRequest;
@@ -28,9 +29,28 @@ class HeroSectionController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreHeroSectionRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'gambar' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'tagline' => 'required|string|max:255',
+            'promosi' => 'required|string|max:255',
+            'tgl' => 'required|string',
+        ]);
+
+        $gambarName = time() . '.' . $request->gambar->extension();
+        $request->gambar->move(public_path('uploads'), $gambarName);
+
+        $userId = Auth::id();
+
+        $hero = new HeroSection();
+        $hero->banner = $gambarName;
+        $hero->tagline = $request->tagline;
+        $hero->promosi = $request->promosi;
+        $hero->tgl = $request->tgl;
+        $hero->user_id = $userId;
+        $hero->save();
+        return redirect()->route('HeroSectionSetting')->with('success', 'Hero Section berhasil disimpan.');
     }
 
     /**
--- app/Models/HeroSection.php
+++ app/Models/HeroSection.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class HeroSection extends Model
 {
     use HasFactory;
+    protected $table = 'hero';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
