117d9b77b43bef6b9a69339157bed2a28b63e25a
Bayu <bayurz1001@gmail.com>
2024-05-13 15:22:00 +0700

    up

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -119,88 +119,55 @@
                             </div>
                             <div class="recent-post-area">
                                 <div class="recent-items">
-                                    <div class="recent-thumb">
-                                        <img src="public/assets/img/news/pp3.jpg" alt="img">
-                                    </div>
-                                    <div class="recent-content">
-                                        <ul>
-                                            <li>
-                                                <i class="fa-solid fa-calendar-days"></i>
-                                                18 Dec, 2024
-                                            </li>
-                                        </ul>
-                                        <h6>
-                                            <a href="news-details.html">
-                                                That Jerk Form Finance <br>
-                                                Really Me
-                                            </a>
-                                        </h6>
-                                    </div>
-                                </div>
-                                <div class="recent-items">
-                                    <div class="recent-thumb">
-                                        <img src="public/assets/img/news/pp4.jpg" alt="img">
-                                    </div>
-                                    <div class="recent-content">
-                                        <ul>
-                                            <li>
-                                                <i class="fa-solid fa-calendar-days"></i>
-                                                18 Dec, 2024
-                                            </li>
-                                        </ul>
-                                        <h6>
-                                            <a href="news-details.html">
-                                                How to keep Chidden Safe <br>
-                                                Online In Simple
-                                            </a>
-                                        </h6>
-                                    </div>
-                                </div>
-                                <div class="recent-items">
-                                    <div class="recent-thumb">
-                                        <img src="public/assets/img/news/pp5.jpg" alt="img">
-                                    </div>
-                                    <div class="recent-content">
-                                        <ul>
-                                            <li>
-                                                <i class="fa-solid fa-calendar-days"></i>
-                                                18 Dec, 2024
-                                            </li>
-                                        </ul>
-                                        <h6>
-                                            <a href="news-details.html">
-                                                Form Without Content <br>
-                                                Style Without
-                                            </a>
-                                        </h6>
+                                    @foreach ($artikel as $article)
+                                        <div class="recent-thumb">
+                                            <img src="{{ asset('public/uploads/' . $article->banner) }}" alt="img">
+                                        </div>
+                                    @endforeach
+                                    <div class="recent-items">
+                                        <div class="recent-thumb">
+                                            <img src="public/assets/img/news/pp5.jpg" alt="img">
+                                        </div>
+                                        <div class="recent-content">
+                                            <ul>
+                                                <li>
+                                                    <i class="fa-solid fa-calendar-days"></i>
+                                                    18 Dec, 2024
+                                                </li>
+                                            </ul>
+                                            <h6>
+                                                <a href="news-details.html">
+                                                    Form Without Content <br>
+                                                    Style Without
+                                                </a>
+                                            </h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="single-sidebar-widget">
-                            <div class="wid-title">
-                                <h3>Tags</h3>
-                            </div>
-                            <div class="news-widget-categories">
-                                <div class="tagcloud">
-                                    <a href="news-standard.html">Time-Table</a>
-                                    <a href="news-details.html">Children</a>
-                                    <a href="news-details.html">Examination</a>
-                                    <a href="news-details.html">Class</a>
-                                    <a href="news-details.html">Sports</a>
-                                    <a href="news-details.html">Canteen</a>
+                            <div class="single-sidebar-widget">
+                                <div class="wid-title">
+                                    <h3>Tags</h3>
+                                </div>
+                                <div class="news-widget-categories">
+                                    <div class="tagcloud">
+                                        <a href="news-standard.html">Time-Table</a>
+                                        <a href="news-details.html">Children</a>
+                                        <a href="news-details.html">Examination</a>
+                                        <a href="news-details.html">Class</a>
+                                        <a href="news-details.html">Sports</a>
+                                        <a href="news-details.html">Canteen</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
     </section>
     <!-- Whatsapp popup -->
-    <div id="whatsapp-popup"
-        style="display: none; position: fixed; bottom: 20px; right: 20px; z-index: 9999; cursor: move;"
+    <div id="whatsapp-popup" style="display: none; position: fixed; bottom: 20px; right: 20px; z-index: 9999; cursor: move;"
         ontouchstart="handleTouchStart(event)" ontouchmove="handleTouchMove(event)">
         <a href="https://wa.me/6281266187125?" target="_blank">
             <img src="public/assets/img/breadcrumb-shape/Whatsapp.png" alt="WhatsApp Icon"
