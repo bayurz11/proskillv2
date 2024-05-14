d85511bec0c854fe69d1c484b2582de83fdda4a5
Bayu <bayurz1001@gmail.com>
2024-05-14 20:15:22 +0700

    up

--- resources/views/kelasOnline.blade.php
+++ resources/views/kelasOnline.blade.php
@@ -46,8 +46,8 @@
     <section class="clases-section section-padding">
         <div class="container">
             <div class="row g-4">
-                <div class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".3s">
-                    @foreach ($kelasOnline as $kelasonline)
+                @foreach ($kelasOnline as $kelasonline)
+                    <div class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".3s">
                         <div class="clases-items mt-0">
                             <div class="clases-bg style-2"></div>
                             <div class="clases-image">
@@ -74,8 +74,8 @@
                                 </ul>
                             </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </section>
