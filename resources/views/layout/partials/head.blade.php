af2cbabd377bfaee978a78fdcffbde911b5d003a
Bayu <bayurz1001@gmail.com>
2024-05-14 14:34:24 +0700

    up

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -1,4 +1,24 @@
-<link rel="shortcut icon" href="public/assets/img/favicon.png" />
+<link rel="shortcut icon" href="{{ asset('public/assets/img/favicon.png') }}" />
+<!--<< Bootstrap min.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/bootstrap.min.css') }}">
+<!--<< All Min Css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/all.min.css') }}">
+<!--<< Animate.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/animate.css') }}">
+<!--<< Icomoon.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/icomoon.css') }}">
+<!--<< Magnific Popup.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/magnific-popup.css') }}">
+<!--<< MeanMenu.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/meanmenu.css') }}">
+<!--<< Swiper Bundle.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/swiper-bundle.min.css') }}">
+<!--<< Nice Select.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/nice-select.css') }}">
+<!--<< Main.css >>-->
+<link rel="stylesheet" href="{{ asset('public/assets/css/main.css') }}">
+
+{{-- <link rel="shortcut icon" href="public/assets/img/favicon.png" />
 <!--<< Bootstrap min.css >>-->
 <link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
 <!--<< All Min Css >>-->
@@ -16,4 +36,4 @@
 <!--<< Nice Select.css >>-->
 <link rel="stylesheet" href="public/assets/css/nice-select.css">
 <!--<< Main.css >>-->
-<link rel="stylesheet" href="public/assets/css/main.css">
+<link rel="stylesheet" href="public/assets/css/main.css"> --}}
