bf0f51ea6988a0843167c767d8bde868a6b1a6b9
Bayu <bayurz1001@gmail.com>
2024-05-14 16:48:38 +0700

    up

--- app/Models/KelasTatapMuka.php
+++ app/Models/KelasTatapMuka.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class KelasTatapMuka extends Model
 {
     use HasFactory;
+
+
+    use HasFactory;
+    protected $table = 'kelas_offline';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
