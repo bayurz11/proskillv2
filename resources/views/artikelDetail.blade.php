2a9f75ea5999452cc5d7729a5691f06bb4eb3f54
Bayu <bayurz1001@gmail.com>
2024-05-14 14:52:49 +0700

    up

--- resources/views/artikelDetail.blade.php
+++ resources/views/artikelDetail.blade.php
@@ -83,7 +83,7 @@
                                     </li>
                                 </ul>
                                 <h3>{{ $artikel->title }} </h3>
-
+                                <img src="{{ asset('public/uploads/' . $article->banner) }}">
                                 <p class="mb-3">
                                     Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
                                     of
