728cd8d213fd97abb30ac69cec5499fb91ea2f94
Bayu <bayurz1001@gmail.com>
2024-05-14 14:35:37 +0700

    up

--- resources/views/layout/partials/footer-scripts.blade.php
+++ resources/views/layout/partials/footer-scripts.blade.php
@@ -1,4 +1,27 @@
 <!--<< All JS Plugins >>-->
+<script src="{{ asset('public/assets/js/jquery-3.7.1.min.js') }}"></script>
+<!--<< Viewport Js >>-->
+<script src="{{ asset('public/assets/js/viewport.jquery.js') }}"></script>
+<!--<< Bootstrap Js >>-->
+<script src="{{ asset('public/assets/js/bootstrap.bundle.min.js') }}"></script>
+<!--<< Nice Select Js >>-->
+<script src="{{ asset('public/assets/js/jquery.nice-select.min.js') }}"></script>
+<!--<< Waypoints Js >>-->
+<script src="{{ asset('public/assets/js/jquery.waypoints.js') }}"></script>
+<!--<< Counterup Js >>-->
+<script src="{{ asset('public/assets/js/jquery.counterup.min.js') }}"></script>
+<!--<< Swiper Slider Js >>-->
+<script src="{{ asset('public/assets/js/swiper-bundle.min.js') }}"></script>
+<!--<< MeanMenu Js >>-->
+<script src="{{ asset('public/assets/js/jquery.meanmenu.min.js') }}"></script>
+<!--<< Magnific Popup Js >>-->
+<script src="{{ asset('public/assets/js/jquery.magnific-popup.min.js') }}"></script>
+<!--<< Wow Animation Js >>-->
+<script src="{{ asset('public/assets/js/wow.min.js') }}"></script>
+<!--<< Main.js >>-->
+<script src="{{ asset('public/assets/js/main.js') }}"></script>
+
+{{-- <!--<< All JS Plugins >>-->
 <script src="public/assets/js/jquery-3.7.1.min.js"></script>
 <!--<< Viewport Js >>-->
 <script src="public/assets/js/viewport.jquery.js"></script>
@@ -19,4 +42,4 @@
 <!--<< Wow Animation Js >>-->
 <script src="public/assets/js/wow.min.js"></script>
 <!--<< Main.js >>-->
-<script src="public/assets/js/main.js"></script>
+<script src="public/assets/js/main.js"></script> --}}
