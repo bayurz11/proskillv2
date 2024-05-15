e586035cfcc4950d5ad07d7975f545809ac05fbb
Bayu <bayurz1001@gmail.com>
2024-05-15 08:54:43 +0700

    up

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -97,104 +97,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="swiper-slide">
-                        <div class="event-box-items mt-0 box-shadow style-bottom">
-                            <div class="event-image">
-                                <img src="assets/img/event/02.jpg" alt="event-img">
-                                <div class="event-shape">
-                                    <img src="assets/img/event/shape.png" alt="shape-img">
-                                </div>
-                                <ul class="post-date">
-                                    <li>
-                                        <img src="assets/img/event/calender.svg" alt="img" class="me-2">
-                                        Feb 20, 2024
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="event-content">
-                                <ul>
-                                    <li>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            viewBox="0 0 16 16" fill="none">
-                                            <path
-                                                d="M12.7847 1.98206C11.5066 0.703906 9.80717 0 7.99961 0C6.19205 0 4.49261 0.703906 3.21448 1.98206C1.93633 3.26025 1.23242 4.95962 1.23242 6.76716C1.23242 10.4238 4.68986 13.4652 6.54733 15.0991C6.80545 15.3262 7.02836 15.5223 7.20595 15.6882C7.42845 15.896 7.71405 15.9999 7.99958 15.9999C8.28517 15.9999 8.5707 15.896 8.79324 15.6882C8.97083 15.5223 9.19374 15.3262 9.45186 15.0991C11.3093 13.4652 14.7668 10.4238 14.7668 6.76716C14.7667 4.95962 14.0629 3.26025 12.7847 1.98206ZM8.8328 14.3954C8.56902 14.6275 8.34124 14.8279 8.15342 15.0033C8.06714 15.0838 7.93202 15.0838 7.8457 15.0033C7.65792 14.8278 7.43011 14.6274 7.16633 14.3954C5.42008 12.8593 2.16961 9.99997 2.16961 6.76719C2.16961 3.55256 4.78489 0.937281 7.99955 0.937281C11.2142 0.937281 13.8295 3.55256 13.8295 6.76719C13.8295 9.99997 10.579 12.8593 8.8328 14.3954Z"
-                                                fill="#F39F5F" />
-                                            <path
-                                                d="M7.9998 3.5293C6.35539 3.5293 5.01758 4.86708 5.01758 6.51148C5.01758 8.15589 6.35539 9.49367 7.9998 9.49367C9.6442 9.49367 10.982 8.15589 10.982 6.51148C10.982 4.86708 9.6442 3.5293 7.9998 3.5293ZM7.9998 8.55639C6.8722 8.55639 5.95483 7.63902 5.95483 6.51145C5.95483 5.38389 6.8722 4.46652 7.9998 4.46652C9.12739 4.46652 10.0447 5.38389 10.0447 6.51145C10.0447 7.63902 9.12739 8.55639 7.9998 8.55639Z"
-                                                fill="#F39F5F" />
-                                        </svg>
-                                        <span>6391 Elgin St. Celina, USA</span>
-                                    </li>
-                                </ul>
-                                <h3>
-                                    <a href="event-details.html">The complete web developer guideline 2024</a>
-                                </h3>
-                                <div class="event-author">
-                                    <a href="event-details.html" class="theme-btn">Get Ticket <i
-                                            class="fa-solid fa-arrow-right-long"></i></a>
-                                    <div class="author-ratting">
-                                        <span>(10 Review)</span>
-                                        <div class="star">
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star color-1"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="event-box-items mt-0 box-shadow style-bottom">
-                            <div class="event-image">
-                                <img src="assets/img/event/03.jpg" alt="event-img">
-                                <div class="event-shape">
-                                    <img src="assets/img/event/shape.png" alt="shape-img">
-                                </div>
-                                <ul class="post-date">
-                                    <li>
-                                        <img src="assets/img/event/calender.svg" alt="img" class="me-2">
-                                        Mar 26, 2024
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="event-content">
-                                <ul>
-                                    <li>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            viewBox="0 0 16 16" fill="none">
-                                            <path
-                                                d="M12.7847 1.98206C11.5066 0.703906 9.80717 0 7.99961 0C6.19205 0 4.49261 0.703906 3.21448 1.98206C1.93633 3.26025 1.23242 4.95962 1.23242 6.76716C1.23242 10.4238 4.68986 13.4652 6.54733 15.0991C6.80545 15.3262 7.02836 15.5223 7.20595 15.6882C7.42845 15.896 7.71405 15.9999 7.99958 15.9999C8.28517 15.9999 8.5707 15.896 8.79324 15.6882C8.97083 15.5223 9.19374 15.3262 9.45186 15.0991C11.3093 13.4652 14.7668 10.4238 14.7668 6.76716C14.7667 4.95962 14.0629 3.26025 12.7847 1.98206ZM8.8328 14.3954C8.56902 14.6275 8.34124 14.8279 8.15342 15.0033C8.06714 15.0838 7.93202 15.0838 7.8457 15.0033C7.65792 14.8278 7.43011 14.6274 7.16633 14.3954C5.42008 12.8593 2.16961 9.99997 2.16961 6.76719C2.16961 3.55256 4.78489 0.937281 7.99955 0.937281C11.2142 0.937281 13.8295 3.55256 13.8295 6.76719C13.8295 9.99997 10.579 12.8593 8.8328 14.3954Z"
-                                                fill="#F39F5F" />
-                                            <path
-                                                d="M7.9998 3.5293C6.35539 3.5293 5.01758 4.86708 5.01758 6.51148C5.01758 8.15589 6.35539 9.49367 7.9998 9.49367C9.6442 9.49367 10.982 8.15589 10.982 6.51148C10.982 4.86708 9.6442 3.5293 7.9998 3.5293ZM7.9998 8.55639C6.8722 8.55639 5.95483 7.63902 5.95483 6.51145C5.95483 5.38389 6.8722 4.46652 7.9998 4.46652C9.12739 4.46652 10.0447 5.38389 10.0447 6.51145C10.0447 7.63902 9.12739 8.55639 7.9998 8.55639Z"
-                                                fill="#F39F5F" />
-                                        </svg>
-                                        <span>6391 Elgin St. Celina, USA</span>
-                                    </li>
-                                </ul>
-                                <h3>
-                                    <a href="event-details.html">Gathering & welcome speech marketing strategy</a>
-                                </h3>
-                                <div class="event-author">
-                                    <a href="event-details.html" class="theme-btn">Get Ticket <i
-                                            class="fa-solid fa-arrow-right-long"></i></a>
-                                    <div class="author-ratting">
-                                        <span>(10 Review)</span>
-                                        <div class="star">
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star"></i>
-                                            <i class="fas fa-star color-1"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
                 <div class="swiper-dot text-center mb-55">
                     <div class="dot"></div>
@@ -204,8 +107,7 @@
     </section>
 
     <!-- Whatsapp popup -->
-    <div id="whatsapp-popup"
-        style="display: none; position: fixed; bottom: 20px; right: 20px; z-index: 9999; cursor: move;"
+    <div id="whatsapp-popup" style="display: none; position: fixed; bottom: 20px; right: 20px; z-index: 9999; cursor: move;"
         ontouchstart="handleTouchStart(event)" ontouchmove="handleTouchMove(event)">
         <a href="https://wa.me/6281266187125?" target="_blank">
             <img src="public/assets/img/breadcrumb-shape/Whatsapp.png" alt="WhatsApp Icon"
