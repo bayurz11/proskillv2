d70b8e29da989acd54cdbb6f67d8a1d877849338
Bayu <bayurz1001@gmail.com>
2024-05-15 09:24:25 +0700

    up

--- resources/views/layout/partials/admin/nav_admin.blade.php
+++ resources/views/layout/partials/admin/nav_admin.blade.php
@@ -63,7 +63,7 @@
                 </div>
             </li>
             <li class="nav-item">
-                <a href="pages/apps/calendar.html" class="nav-link">
+                <a href="{{ route('event_setting') }}" class="nav-link">
                     <i class="link-icon" data-feather="calendar"></i>
                     <span class="link-title">Event</span>
                 </a>
