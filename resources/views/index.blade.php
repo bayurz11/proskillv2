07cfa948a63c03bdea84cb5a11101b3edde3d6b1
Bayu <bayurz1001@gmail.com>
2024-05-15 15:03:57 +0700

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -537,53 +537,6 @@
             <div class="row">
                 <div class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".3s">
                     @foreach ($event as $events)
-                        <div class="swiper-slide">
-                            <div class="event-box-items mt-0 box-shadow style-bottom">
-                                <div class="event-image">
-                                    <img src="{{ asset('public/uploads/' . $events->banner) }}">
-                                    <div class="event-shape">
-                                        <img src="{{ asset('public/uploads/' . $events->banner) }}">
-                                    </div>
-                                    <ul class="post-date">
-                                        <li>
-                                            <img src="{{ asset('public/assets/img/event/calender.svg') }}" alt="img"
-                                                class="me-2">
-                                            {{ $events->tgl }}
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="event-content">
-                                    <ul>
-                                        <li>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                viewBox="0 0 16 16" fill="none">
-                                                <path
-                                                    d="M12.7847 1.98206C11.5066 0.703906 9.80717 0 7.99961 0C6.19205 0 4.49261 0.703906 3.21448 1.98206C1.93633 3.26025 1.23242 4.95962 1.23242 6.76716C1.23242 10.4238 4.68986 13.4652 6.54733 15.0991C6.80545 15.3262 7.02836 15.5223 7.20595 15.6882C7.42845 15.896 7.71405 15.9999 7.99958 15.9999C8.28517 15.9999 8.5707 15.896 8.79324 15.6882C8.97083 15.5223 9.19374 15.3262 9.45186 15.0991C11.3093 13.4652 14.7668 10.4238 14.7668 6.76716C14.7667 4.95962 14.0629 3.26025 12.7847 1.98206ZM8.8328 14.3954C8.56902 14.6275 8.34124 14.8279 8.15342 15.0033C8.06714 15.0838 7.93202 15.0838 7.8457 15.0033C7.65792 14.8278 7.43011 14.6274 7.16633 14.3954C5.42008 12.8593 2.16961 9.99997 2.16961 6.76719C2.16961 3.55256 4.78489 0.937281 7.99955 0.937281C11.2142 0.937281 13.8295 3.55256 13.8295 6.76719C13.8295 9.99997 10.579 12.8593 8.8328 14.3954Z"
-                                                    fill="#F39F5F" />
-                                                <path
-                                                    d="M7.9998 3.5293C6.35539 3.5293 5.01758 4.86708 5.01758 6.51148C5.01758 8.15589 6.35539 9.49367 7.9998 9.49367C9.6442 9.49367 10.982 8.15589 10.982 6.51148C10.982 4.86708 9.6442 3.5293 7.9998 3.5293ZM7.9998 8.55639C6.8722 8.55639 5.95483 7.63902 5.95483 6.51145C5.95483 5.38389 6.8722 4.46652 7.9998 4.46652C9.12739 4.46652 10.0447 5.38389 10.0447 6.51145C10.0447 7.63902 9.12739 8.55639 7.9998 8.55639Z"
-                                                    fill="#F39F5F" />
-                                            </svg>
-                                            <span>{{ $events->lokasi }}</span>
-                                        </li>
-                                    </ul>
-                                    <h3>
-                                        <a href="{{ route('event_detail', ['id' => $events->id]) }}">
-                                            {{ $events->name_event }}</a>
-                                    </h3>
-                                    <div class="event-author">
-                                        <a href="{{ route('event_detail', ['id' => $events->id]) }}"
-                                            class="theme-btn">Detail Event <i
-                                                class="fa-solid fa-arrow-right-long"></i></a>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-                <div class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".3s">
-                    @foreach ($event as $events)
                         <div class="event-box-items">
                             <div class="event-image">
                                 <img src="{{ asset('public/uploads/' . $events->banner) }}" alt="event-img">
