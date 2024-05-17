71c8c9b2339c0f8a94850fcbdb9556e4c1a4c054
Bayu <bayurz1001@gmail.com>
2024-05-17 13:57:17 +0700

    up

--- resources/views/layout/partials/admin/nav_admin.blade.php
+++ resources/views/layout/partials/admin/nav_admin.blade.php
@@ -62,12 +62,7 @@
                     </ul>
                 </div>
             </li>
-            <li class="nav-item">
-                <a href="{{ route('event_setting') }}" class="nav-link">
-                    <i class="link-icon" data-feather="calendar"></i>
-                    <span class="link-title">Event</span>
-                </a>
-            </li>
+
             <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#aboutUs" role="button" aria-expanded="false"
                     aria-controls="aboutUs">
