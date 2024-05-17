d2a354f5974148d84983209d98130157d68647e1
Bayu <bayurz1001@gmail.com>
2024-05-17 15:10:19 +0700

    up

--- resources/views/admin/galery.blade.php
+++ resources/views/admin/galery.blade.php
@@ -88,6 +88,84 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="linkModal" tabindex="-1" aria-labelledby="linkModal" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <form action="{{ route('galery_store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Tambah Galery</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="btn-close"></button>
+                        </div>
+                        <div class="modal-body">
+
+
+                            <div class="mb-3">
+                                <label for="gambar" class="form-label">Benner/Gambar</label>
+                                <input type="file" accept="image/*" class="form-control" id="gambar" name="gambar">
+                                <small class="text-secondary">Note : Unggah gambar untuk event yang akan
+                                    ditulis</small>
+                            </div>
+                            <img id="preview" src="#" alt="Preview Gambar"
+                                style="max-width: 100%; max-height: 200px; display: none;">
+
+
+                            <div class="mb-3">
+                                <label for="lokasi" class="form-label">Lokasi Event</label>
+                                <input type="text" class="form-control" id="lokasi" name="lokasi">
+                            </div>
+                            <div class="mb-3">
+                                <label for="name_event" class="form-label">nama Event</label>
+                                <input type="text" class="form-control" id="name_event" name="name_event">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="tgl" class="form-label">Tanggal Ditulis</label>
+                                <input type="text" class="form-control" id="tgl" placeholder="tgl"
+                                    name="tgl" readonly>
+                            </div>
+
+                            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                            <script>
+                                $(document).ready(function() {
+                                    $("#gambar").change(function() {
+                                        readURL(this);
+                                    });
+                                    var currentDate = new Date();
+                                    var day = currentDate.getDate();
+                                    var month = currentDate.getMonth();
+                                    var year = currentDate.getFullYear();
+                                    var monthNames = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus",
+                                        "September", "Oktober", "November", "Desember"
+                                    ];
+                                    day = day < 10 ? '0' + day : day;
+                                    var formattedDate = day + ' ' + monthNames[month] + ' ' + year;
+                                    $('#tgl').val(formattedDate);
+                                });
+
+                                function readURL(input) {
+                                    if (input.files && input.files[0]) {
+                                        var reader = new FileReader();
+
+                                        reader.onload = function(e) {
+                                            $('#preview').attr('src', e.target.result).show();
+                                        };
+
+                                        reader.readAsDataURL(input.files[0]);
+                                    }
+                                }
+                            </script>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
 
         <div class="row">
@@ -100,6 +178,11 @@
                             data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="btn-icon-prepend"
                                 data-feather="plus-circle"></i>
                             Tambah Event
+                        </button>
+                        <button type="button" class="btn btn-outline-primary position-absolute top-0 end-0 mt-3 me-3"
+                            data-bs-toggle="modal" data-bs-target="#linkModal"><i class="btn-icon-prepend"
+                                data-feather="plus-circle"></i>
+                            Tambah Link Video
                         </button>
                         <p class="text-muted mb-3">Jumlah Foto : {{ $galery->count() }}</p>
                         <div class="table-responsive">
