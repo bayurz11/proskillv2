c30c538ce98e2e75c154a83bb8f9a5bf32304d12
Bayu <bayurz1001@gmail.com>
2024-05-18 10:37:46 +0700

    up

--- resources/views/kelasOfflinedetail.blade.php
+++ resources/views/kelasOfflinedetail.blade.php
@@ -105,7 +105,7 @@
                     </div>
                     <div class="col-lg-4">
                         <div class="details-list-area">
-                            <h3>Classes includes:</h3>
+                            <h3>Ditel Kelas</h3>
                             <ul class="details-list">
                                 <li>
                                     <span>
