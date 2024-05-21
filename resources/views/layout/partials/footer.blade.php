5c60f0d76c61a226a0885ba5695c78c437d83d1b
Bayu <bayurz1001@gmail.com>
2024-05-21 18:32:56 +0700

    up

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -35,21 +35,27 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="news-details.html">
+                                <a href="{{ route('Kelastatapmuka') }}">
                                     <i class="fa-solid fa-chevron-right"></i>
                                     Kelas Tatap Muka
                                 </a>
                             </li>
                             <li>
-                                <a href="faq.html">
+                                <a href="{{ route('kelasOnline') }}">
                                     <i class="fa-solid fa-chevron-right"></i>
                                     Kelas Online
                                 </a>
                             </li>
                             <li>
-                                <a href="contact.html">
+                                <a href="{{ route('event') }}">
                                     <i class="fa-solid fa-chevron-right"></i>
-                                    Tentang Kami
+                                    Event
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('artikel') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Artikel
                                 </a>
                             </li>
                         </ul>
@@ -58,19 +64,32 @@
                 <div class="col-xl-3 col-lg-4 col-md-6 ps-lg-5 wow fadeInUp" data-wow-delay=".5s">
                     <div class="single-footer-widget style-margin">
                         <div class="widget-head">
-                            <h3>Lainnya</h3>
+                            <h3>Profile</h3>
                         </div>
                         <ul class="list-area">
                             <li>
-                                <a href="program-details.html">
+                                <a href="{{ route('tentangkami') }}">
                                     <i class="fa-solid fa-chevron-right"></i>
-                                    Download Browsur
+                                    Tentang Kami
                                 </a>
                             </li>
                             <li>
-                                <a href="">
+                                <a href="program-details.html">
                                     <i class="fa-solid fa-chevron-right"></i>
-                                    Login
+                                    Contoh Sertifikat
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://drive.google.com/file/d/1FiVmF5_hau_8xRKfvoM8Wel1tUezCzA_/view"
+                                    target="_blank">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Brosur
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('hubungikami') }}">
+                                    <i class="fa-solid fa-chevron-right"></i>
+                                    Hubungi Kami
                                 </a>
                             </li>
 
@@ -92,12 +111,12 @@
                                 <a href="https://www.youtube.com/channel/UCNw50VFi4Rdmv1WpKlWGHUQ" target="_blank"><i
                                         class="fa-brands fa-youtube"></i></a>
                             </div>
-                            <a href="https://wa.me/6281266187125?text=Halo,%20saya%20mau%20konsultasi"
+                            {{-- <a href="https://wa.me/6281266187125?text=Halo,%20saya%20mau%20konsultasi"
                                 data-animation="fadeInUp" data-delay="1.7s" class="theme-btn hover-white"
                                 target="_blank">
                                 Hubungi Admin
                                 <i class="fa-solid fa-arrow-right-long"></i>
-                            </a>
+                            </a> --}}
                         </div>
                     </div>
                 </div>
