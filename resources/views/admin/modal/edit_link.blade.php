ef4b5ce042710b3d548bd2173776a702aac8a784
Bayu <bayurz1001@gmail.com>
2024-05-21 19:18:10 +0700

    up

--- resources/views/admin/modal/edit_link.blade.php
+++ resources/views/admin/modal/edit_link.blade.php
@@ -16,7 +16,7 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keluar</button>
                     <button type="submit" class="btn btn-primary">Simpan</button>
                 </div>
             </form>
