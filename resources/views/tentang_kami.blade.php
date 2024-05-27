d9042af435ce0585f4cc92dd34dd78b7ac3113df
Bayu <bayurz1001@gmail.com>
2024-05-27 08:52:50 +0700

    up

--- resources/views/tentang_kami.blade.php
+++ resources/views/tentang_kami.blade.php
@@ -59,7 +59,9 @@
                     @endphp
                     <div class="col-lg-6 wow fadeInUp" data-wow-delay=".3s">
                         <div class="activities-image-items">
-                            <img src="{{ asset('public/uploads/' . $about->banner) }}">
+                            <img src="{{ $about->banner ? asset('public/uploads/' . $about->banner) : asset('path/to/default/image.jpg') }}"
+                                alt="Banner">
+
 
                             <div class="circle-shape">
                                 <img src="public/assets/img/about/circle.png" alt="shape-img">
