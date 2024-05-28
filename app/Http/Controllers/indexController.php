ced71b1b474cda92d293a1c542f9a9deb11053b6
Bayu <bayurz1001@gmail.com>
2024-05-28 20:29:40 +0700

    up

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -53,7 +53,8 @@ class indexController extends Controller
     public function showArtikel()
     {
         $artikel = Artikel::orderBy('tgl', 'desc')->take(3)->get();
-        $artikels = Artikel::paginate(2);
+        $artikels = Artikel::latest()->take(2)->get();
+        // $artikels = Artikel::paginate(2);
         return view('artikel', compact('artikel', 'artikels'));
     }
     public function artikeldetail($id)
