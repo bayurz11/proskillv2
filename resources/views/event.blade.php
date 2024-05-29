fa0dd62aa1d87d2f8ee150b9fdb26f4f2c816778
Bayu <bayurz1001@gmail.com>
2024-05-29 14:18:07 +0700

    up

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -7,7 +7,7 @@
         .breadcrumb-wrapper {
             background-size: cover;
             background-position: center;
-            padding: 10px 0;
+            padding: 2px 0;
             /* Adjust the padding as needed */
         }
 
