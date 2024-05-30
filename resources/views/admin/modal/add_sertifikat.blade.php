9c7d88b26e8a1e7323e708db97fd9c2a88561946
Bayu <bayurz1001@gmail.com>
2024-05-30 09:25:55 +0700

    up

--- resources/views/admin/modal/add_sertifikat.blade.php
+++ resources/views/admin/modal/add_sertifikat.blade.php
@@ -8,10 +8,17 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
                 </div>
                 <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="gambar" class="form-label">Sertifikat/Gambar</label>
+                        <input type="file" accept="image/*" class="form-control" id="gambar" name="gambar">
+                        <small class="text-secondary">Note : Unggah gambar</small>
+                    </div>
+                    <img id="preview" src="#" alt="Preview Gambar"
+                        style="max-width: 100%; max-height: 200px; display: none;">
 
                     <div class="mb-3">
-                        <label for="link" class="form-label">Link Video</label>
-                        <input type="text" class="form-control" id="link" name="link">
+                        <label for="sertifikat_name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="sertifikat_name" name="sertifikat_name">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -22,3 +29,32 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $("#gambar").change(function() {
+            readURL(this);
+        });
+        var currentDate = new Date();
+        var day = currentDate.getDate();
+        var month = currentDate.getMonth();
+        var year = currentDate.getFullYear();
+        var monthNames = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus",
+            "September", "Oktober", "November", "Desember"
+        ];
+        day = day < 10 ? '0' + day : day;
+        var formattedDate = day + ' ' + monthNames[month] + ' ' + year;
+        $('#tgl').val(formattedDate);
+    });
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('#preview').attr('src', e.target.result).show();
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
