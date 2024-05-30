010ee009d1e50f52d863972bc2a155253fe027a6
Bayu <bayurz1001@gmail.com>
2024-05-30 10:02:55 +0700

    up

--- resources/views/admin/modal/edit_sertifikat.blade.php
+++ resources/views/admin/modal/edit_sertifikat.blade.php
@@ -47,9 +47,10 @@
                     .then(response => response.json())
                     .then(data => {
                         document.getElementById('edit-id').value = data.id;
-                        document.getElementById('sertifikat_name').value = data
+                        document.getElementById('sertifikat_name_edit').value = data
                             .link; // Set nilai input sesuai dengan data.link
-                        document.getElementById('editForm').action = `/data/${data.id}`;
+                        document.getElementById('editForm').action =
+                            `/sertifikat/${data.id}`;
                     })
                     .catch(error => {
                         console.error('Error fetching data:', error);
