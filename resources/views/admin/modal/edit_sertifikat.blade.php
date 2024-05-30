8fed8ea2ce0956393ef190b4d9031b68a93d13ec
Bayu <bayurz1001@gmail.com>
2024-05-30 09:51:32 +0700

    up

--- resources/views/admin/modal/edit_sertifikat.blade.php
+++ resources/views/admin/modal/edit_sertifikat.blade.php
@@ -1,65 +1,47 @@
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <form action="{{ route('sertifikat_store') }}" method="POST" enctype="multipart/form-data">
+            <form id="editForm" action="" method="POST" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Tambah Sertifikat</h5>
+                    <h5 class="modal-title" id="editModalLabel">Edit Sertifikat</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
                 </div>
                 <div class="modal-body">
+                    <input type="hidden" id="edit-id" name="id">
                     <div class="mb-3">
-                        <label for="sertifikat_name" class="form-label">Nama </label>
-                        <input type="text" class="form-control" id="sertifikat_name" name="sertifikat_name">
-                    </div>
-
-                    <div class="mb-3">
-                        <label class="form-label" for="gambar">Sertifikat</label>
-                        <input type="file" accept="image/*" class="form-control" id="gambar" name="gambar">
-                    </div>
-                    <img id="preview" src="#" alt="Preview banner"
-                        style="max-width: 100%; max-height: 200px; display: none;">
-                    <div class="mb-3">
-                        <label for="tgl" class="form-label">Tanggal Ditulis</label>
-                        <input type="text" class="form-control" id="tgl" placeholder="tgl" name="tgl"
-                            readonly>
+                        <label for="link" class="form-label">Link Video</label>
+                        <input type="text" class="form-control" id="link_edit" name="link" value="">
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keluar</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Simpan</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <script>
-    $(document).ready(function() {
-        $("#gambar").change(function() {
-            readURL(this);
+    document.addEventListener('DOMContentLoaded', function() {
+        const editButtons = document.querySelectorAll('.edit-button');
+        editButtons.forEach(button => {
+            button.addEventListener('click', function() {
+                const id = this.getAttribute('data-id');
+                fetch(`/data/${id}/edit`)
+                    .then(response => response.json())
+                    .then(data => {
+                        document.getElementById('edit-id').value = data.id;
+                        document.getElementById('link_edit').value = data
+                            .link; // Set nilai input sesuai dengan data.link
+                        document.getElementById('editForm').action = `/data/${data.id}`;
+                    })
+                    .catch(error => {
+                        console.error('Error fetching data:', error);
+                    });
+            });
         });
-        var currentDate = new Date();
-        var day = currentDate.getDate();
-        var month = currentDate.getMonth();
-        var year = currentDate.getFullYear();
-        var monthNames = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus",
-            "September", "Oktober", "November", "Desember"
-        ];
-        day = day < 10 ? '0' + day : day;
-        var formattedDate = day + ' ' + monthNames[month] + ' ' + year;
-        $('#tgl').val(formattedDate);
     });
-
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function(e) {
-                $('#preview').attr('src', e.target.result).show();
-            };
-
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
 </script>
--- resources/views/admin/sertifikat.blade.php
+++ resources/views/admin/sertifikat.blade.php
@@ -15,54 +15,7 @@
         @include('admin.modal.add_sertifikat')
         @include('admin.modal.edit_sertifikat')
 
-        <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <form id="editForm" action="" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="editModalLabel">Edit Sertifikat</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="btn-close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <input type="hidden" id="edit-id" name="id">
-                            <div class="mb-3">
-                                <label for="link" class="form-label">Link Video</label>
-                                <input type="text" class="form-control" id="link_edit" name="link" value="">
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
 
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                const editButtons = document.querySelectorAll('.edit-button');
-                editButtons.forEach(button => {
-                    button.addEventListener('click', function() {
-                        const id = this.getAttribute('data-id');
-                        fetch(`/data/${id}/edit`)
-                            .then(response => response.json())
-                            .then(data => {
-                                document.getElementById('edit-id').value = data.id;
-                                document.getElementById('link_edit').value = data
-                                    .link; // Set nilai input sesuai dengan data.link
-                                document.getElementById('editForm').action = `/data/${data.id}`;
-                            })
-                            .catch(error => {
-                                console.error('Error fetching data:', error);
-                            });
-                    });
-                });
-            });
-        </script>
 
         <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
