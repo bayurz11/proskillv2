443fcbc2b6c57cd9fa8e35473dbef6f76684e5c5
Bayu <bayurz1001@gmail.com>
2024-05-31 14:30:24 +0700

    up

--- app/Models/PendaftaranSiswa.php
+++ app/Models/PendaftaranSiswa.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class PendaftaranSiswa extends Model
 {
+
+
     use HasFactory;
+    protected $table = 'pendaftaran_siswa';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
