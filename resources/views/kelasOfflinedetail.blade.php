7dbb67365d33731867ec556f1b2b5e85a84d9e46
Bayu <bayurz1001@gmail.com>
2024-06-04 13:37:07 +0700

    up

--- resources/views/kelasOfflinedetail.blade.php
+++ resources/views/kelasOfflinedetail.blade.php
@@ -109,37 +109,15 @@
                                     @endif
                                 </ul>
                             </div>
-                            <div class="details-content">
-                                {{-- <div class="post">
-                                    <span>Kindergarten</span>
-                                </div> --}}
-                                <h2 class="mb-0">{{ $klsoffline->kelas }}</h2>
-                                <div class="details-author-area">
-                                    <div class="author-items">
-                                        <i class="fas fa-user"></i>
-                                        <p>{{ $klsoffline->instruktur }}</p>
-                                    </div>
-                                    {{-- <ul class="class-list">
-                                        <li>
-                                            <i class="fa-regular fa-circle-play me-2"></i>
-                                            30 Classes
-                                        </li>
-                                        <li>
-                                            <i class="fas fa-star me-2"></i>
-                                            3.4 (36 Review)
-                                        </li>
-                                    </ul> --}}
-                                </div>
-                                <h2>Deskripsi Kelas</h2>
-                                <p class="mb-3">
-                                    {{ $klsoffline->deskripsi }}
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="program-details-items">
+                            <div class="details-image">
+                                <img src="{{ asset('public/uploads/' . $klsoffline->banner) }}" alt="img">
 
-                                <h2>Fasilitas Yang Didapat</h2>
-                                {{-- <p>
-                                    Nulla facilisi. Vestibulum tristique sem in eros eleifend imperdiet. Donec quis
-                                    convallis neque. In id lacus pulvinar lacus, eget vulputate lectus. Ut viverra bibendum
-                                    lorem, at tempus nibh mattis in. Sed a massa eget lacus consequat auctor.
-                                </p> --}}
+                            </div>
+                            <div class="details-content">
                                 <ul class="list-items">
                                     @if (!empty($fasilitas))
                                         @foreach ($fasilitas as $item)
